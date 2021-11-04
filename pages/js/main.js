4650821eeff1d9c185a8110159c688d932636efe
Alexandr <90194375+saspovych@users.noreply.github.com>
2021-11-04 21:52:33 +0300

    Add files via upload

--- pages/js/main.js
+++ pages/js/main.js
@@ -14,3 +14,30 @@ function showSpoiler(index, height) {
 	}
 	changePlusOnMinus(index)
 }
+
+
+function rotateTriangle(index) {
+	const triangle = document.querySelectorAll('.overflow')
+	if (triangle[index].classList.contains('unactive')) {
+		triangle[index].classList.remove('unactive')
+		triangle[index].classList.add('active')
+	} else {
+		triangle[index].classList.remove('active')
+		triangle[index].classList.add('unactive')
+	}
+}
+function showHiddenBlock(index, height) {
+	const hiddenBlock = document.querySelectorAll('.hidden_block')
+	if (hiddenBlock[index].classList.contains('hide')) {
+		hiddenBlock[index].classList.remove('hide')
+		hiddenBlock[index].classList.add('show')
+		hiddenBlock[index].style.height = `${height}px`
+		hiddenBlock[index].style.transition = 'height 1.5s ease'
+	} else {
+		hiddenBlock[index].classList.remove('show')
+		hiddenBlock[index].classList.add('hide')
+		hiddenBlock[index].style.height = '0px'
+		hiddenBlock[index].style.transition = 'height 1.5s ease'
+	}
+	rotateTriangle(index)
+}
